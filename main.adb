5816e48114aef70e49c4428d93ca431f7b459a84
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-21 14:49:17 +0200

    Implement main procedure for WLPC algorithm

---
+++ main.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Warped_LPC;
+
+procedure Main is
+begin
+   Put_Line ("Warped Linear Predictive Coding (WLPC) Algorithm");
+   Put_Line ("Library compiled successfully. Execute 'make test' to run the Verification Suite.");
+end Main;
